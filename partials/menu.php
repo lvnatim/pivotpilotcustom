43a34efa912edf8887ffc47d51cbe39bdbbef24e
Josh Coles <josh@joshcoles.com>
2017-03-14 12:27:54 -0700

    Completed services page for mobile.

--- partials/menu.php
+++ partials/menu.php
@@ -26,35 +26,35 @@
           <li id="menu-branding">
           <h1>branding &amp; logo design</h1>
             <ul class="submenu-2">
-              <li><a href="http://google.com"><h1>branded stationary</h1></a></li>
-              <li><h1>brand development</h1></li>
+              <li><a href="/pnp3/services?category=11"><h1>branded stationary</h1></a></li>
+              <li><a href="/pnp3/services?category=12"><h1>brand development</h1></a></li>
             </ul>
           </li>
           <li id="menu-website"><h1>website design</h1>
             <ul class="submenu-2">
-              <li><h1>website build</h1></li>
-              <li><h1>Google Adwords &amp; SEO</h1></li>
-              <li><h1>interactive animation</h1></li>
+              <li><a href="/pnp3/services?category=4"<h1>website build</h1></a></li>
+              <li><a href="/pnp3/services?category=16"<h1>Google Adwords &amp; SEO</h1></a></li>
+              <li><a href="/pnp3/services?category=18"<h1>interactive animation</h1></a></li>
             </ul>
           </li>
           <li id="menu-print"><h1>print &amp; packaging</h1>
             <ul class="submenu-2">
-              <li><h1>branded stationary</h1></li>
-              <li><h1>marketing materials</h1></li>
-              <li><h1>product packaging</h1></li>
+              <li><a href="/pnp3/services?category=23"><h1>branded stationary</h1></a></li>
+              <li><a href="/pnp3/services?category=19"><h1>marketing materials</h1></a></li>
+              <li><a href="/pnp3/services?category=20"><h1>product packaging</h1></a></li>
             </ul>
           </li>
           <li id="menu-digital"><h1>digital marketing</h1>
             <ul class="submenu-2">
-              <li><h1>social media campaigns </h1></li>
-              <li><h1>corporate videos</h1></li>
-              <li><h1>Google Adwords &amp; SEO</h1></li>
+              <li><a href="/pnp3/services?category=13"><h1>social media campaigns</h1></a></li>
+              <li><a href="/pnp3/services?category=14"><h1>corporate videos</h1></a></li>
+              <li><a href="/pnp3/services?category=15"><h1>Google Adwords &amp; SEO</h1></a></li>
             </ul>
           </li>
           <li id="menu-film"><h1>film &amp; animation</h1>
             <ul class="submenu-2">
-              <li><h1>corporate videos</h1></li>
-              <li><h1>interactive animation</h1></li>
+              <li><a href="/pnp3/services?category=21"><h1>corporate videos</h1></a></li>
+              <li><a href="/pnp3/services?category=22"><h1>interactive animation</h1></a></li>
             </ul>
           </li>
         </ul>
