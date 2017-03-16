e44703faee6424fc765b009d94035e31498b5170
Josh Coles <josh@joshcoles.com>
2017-03-16 10:34:48 -0700

    Added some rules for tablet and small desktop for services page. Added some animations so that text slides in from bottom on overlay on services page.

--- page-services.php
+++ page-services.php
@@ -20,59 +20,68 @@ $service_tagline = get_field('service_tagline', 'services_' . $category);
     <h3><?php echo $service_tagline ?></h3>
     <p><?php echo $service_description ?></p>
   </div>
-  <h3>Why does it matter?</h3>
-  <div class="services-icons-slider">
 
-  <?php
-  $currentNumber = 1;
-  for ($i = 1; $i < 7; $i++) {
+  <div class="services-snippets-container">
+    <h3>Why does it matter?</h3>
+    <div class="services-icons-slider">
 
-    $snippet_excerpt = get_field('snippet_excerpt_' . $currentNumber, 'services_' . $category);
-    $snippet_description = get_field('snippet_description_' . $currentNumber, 'services_' . $category);
-    $snippet_icon = get_field('snippet_icon_' . $currentNumber, 'services_' . $category);
-    $snippet_icon_inverse = get_field('snippet_icon_inverse_' . $currentNumber, 'services_' . $category);
-    // $selected_logo = get_field('selected_logo_' . $currentNumber, 'services_' . $category);
-    // $selected_logo_link = get_field('link_for_selected_logo_' . $currentNumber, 'services_' . $category);
+    <?php
+    $currentNumber = 1;
+    for ($i = 1; $i < 7; $i++) {
 
-    if (strlen($snippet_description) > 0 ) {
-        echo "<div class='services-icon-post'>
-        <img src=" . $snippet_icon . ">
-        <h3>" . $snippet_excerpt . "</h3>
-        <div class='more-info-plus'>
-          <p>+</p>
-        </div>
-        <div class='service-overlay'>
-          <div id='service-overlay-exit'>
-            <span></span>
-            <span></span>
+      $snippet_excerpt = get_field('snippet_excerpt_' . $currentNumber, 'services_' . $category);
+      $snippet_description = get_field('snippet_description_' . $currentNumber, 'services_' . $category);
+      $snippet_icon = get_field('snippet_icon_' . $currentNumber, 'services_' . $category);
+      $snippet_icon_inverse = get_field('snippet_icon_inverse_' . $currentNumber, 'services_' . $category);
+      // $selected_logo = get_field('selected_logo_' . $currentNumber, 'services_' . $category);
+      // $selected_logo_link = get_field('link_for_selected_logo_' . $currentNumber, 'services_' . $category);
+
+      if (strlen($snippet_description) > 0 ) {
+          echo "<div class='services-icon-post'>
+          <img src=" . $snippet_icon . ">
+          <h3>" . $snippet_excerpt . "</h3>
+          <div class='more-info-plus'>
+            <p>+</p>
           </div>
-          <div class='service-overlay-content'>
-            <img class='img-responsive' src='" . $snippet_icon_inverse . "'>
-            <h1>" . $service_name . "</h1>
-            <h3>" . $snippet_excerpt . "</h3>
-            <p> " . $snippet_description . "</p>
+          <div class='service-overlay'>
+            <div id='service-overlay-exit'>
+              <span></span>
+              <span></span>
+            </div>
+            <div class='service-overlay-content'>
+              <div class='snippet-icon-container'>
+                <img class='img-responsive' src='" . $snippet_icon_inverse . "'>
+              </div>
+              <div class='snippet-content-container'>
+                <h1>" . $service_name . "</h1>
+                <h3>" . $snippet_excerpt . "</h3>
+                <p> " . $snippet_description . "</p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>";
+        </div>";
+      }
+      $currentNumber += 1;
     }
-    $currentNumber += 1;
-  }
-  ?>
+    ?>
+    </div>
   </div>
 
   <div class="selected-logos">
     <h3>Selected Logos</h3>
+      <div class="image-container">
 
-    <?php
-      $image_counter= 1;
-      for ($i = 1; $i < 7; $i++) {
-      $selected_logo = get_field('selected_logo_' . $image_counter, 'services_' . $category);
-      $selected_logo_link = get_field('link_for_selected_logo_' . $image_counter, 'services_' . $category);
-      echo "<a href='" . $selected_logo_link .  "''><img class='img-responsive' src=" . $selected_logo . "></a>";
-      $image_counter += 1;
-      }
-    ?>
+      <?php
+        $image_counter= 1;
+        for ($i = 1; $i < 7; $i++) {
+        $selected_logo = get_field('selected_logo_' . $image_counter, 'services_' . $category);
+        $selected_logo_link = get_field('link_for_selected_logo_' . $image_counter, 'services_' . $category);
+        echo "<a href='" . $selected_logo_link .  "''><img class='img-responsive' src=" . $selected_logo . "></a>";
+        $image_counter += 1;
+        }
+      ?>
 
+    </div>
   </div>
 </section>
 
@@ -81,14 +90,19 @@ $service_tagline = get_field('service_tagline', 'services_' . $category);
 <script>
 jQuery(document).ready(function($){
   $('.more-info-plus').on('click', function() {
+
     $(this).next().addClass('active');
+    $('.service-overlay-content').animate({'padding-top': '50px'}, 500);
+
   });
+
+
 
   // Currently clicking anywhere on overlay removes 'active' class.
 
   $('.service-overlay').on('click', function() {
-    console.log("you're clicking me");
     $('.service-overlay').removeClass('active');
+    $('.service-overlay-content').animate({'padding-top': '800px'}, 200);
   })
 });
 </script>
